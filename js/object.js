0418ab9c0a57dbf8dc934a94fccb5b532e29e085
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-10 04:27:51 -0700

    Define an object

--- js/object.js
+++ js/object.js
@@ -9,5 +9,8 @@ an object is an entity having a state and behavior (properties and methods)
 const courses = {
     title: "object",
     definition: "an object is an entity having a state and behavior (properties and methods) ",
-    creationDate: "10/04/2023"
+    creationDate: "10/04/2023",
+    created: function () {
+        return this.title + "" + "was craeted at " + this.creationDate
+    }
 }

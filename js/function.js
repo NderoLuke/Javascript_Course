9a92e97c033acb7d143f97749a2bbe9d0b833bef
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-10 10:51:50 -0700

    Define a function

---
+++ js/function.js
@@ -0,0 +1,13 @@
+/*
+a function is a block of code designed to perform a particular task.
+
+a javascript function is executed when "something" called it.
+*/
+
+
+
+function Sum(num1, num2) {
+    return num1 + num2;
+}
+
+console.log(Sum(23, 8));

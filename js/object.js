c2cf5bfb0b7071b8a0d24b5e668f7fddc0de46cd
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-10 04:39:13 -0700

    Call object function

--- js/object.js
+++ js/object.js
@@ -11,6 +11,9 @@ const courses = {
     definition: "an object is an entity having a state and behavior (properties and methods) ",
     creationDate: "10/04/2023",
     created: function () {
-        return this.title + "" + "was craeted at " + this.creationDate
+        return this.title + "" + " was created at  " + this.creationDate
     }
 }
+
+let object = document.querySelector(".object");
+object.innerHTML = courses.created();

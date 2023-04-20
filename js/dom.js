15c424faa3ddcb0bd4d792c9592ea1b6f8911e67
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-20 09:14:16 -0700

    Add DOM note

--- js/dom.js
+++ js/dom.js
@@ -1,1 +1,13 @@
 document.querySelector(".h").innerHTML = "Bye";
+
+/* The HTML DOM is a standard for how to get, change, add and delete HTML elements
+
+in the exapmle above querSelector: is a method/action wheras innerHTML: is a property
+
+*/
+let subTitle = document.createElement("h4");
+subTitle.innerHTML = "This is subTitle"
+let h2 = document.querySelector(".object").appendChild(subTitle);
+
+console.log(subTitle);
+h2.appendChild(subTitle);

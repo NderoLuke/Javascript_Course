704a6e3d7eec591f43e26ade9f7591bc24101989
SAHA ELECTRONICS <nderoluke1@outlook.com>
2023-04-21 07:20:20 -0700

    merge dev

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,22 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // HTML DOM document object is owner of all other object in your webpage
 
 =======
 >>>>>>> dev
 document.querySelector(".h").innerHTML = "Bye";
+=======
+document.querySelector(".h").innerHTML = "Bye";
+
+/* The HTML DOM is a standard for how to get, change, add and delete HTML elements
+
+in the exapmle above querSelector: is a method/action wheras innerHTML: is a property
+
+*/
+let subTitle = document.createElement("h4");
+subTitle.innerHTML = "This is subTitle"
+let h2 = document.querySelector(".object").appendChild(subTitle);
+
+console.log(subTitle);
+h2.appendChild(subTitle);
+>>>>>>> dev
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 Javascript course
+Modern javascript  tutorial
+Basic
